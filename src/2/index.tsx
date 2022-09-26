7f3446ab4c3322c4b772b67fae6070b9c4dc3733
Arwa mohammed <arwa.elmowafy1996@gmail.com>
2022-09-26 17:19:16 +0200

    solve(task2): create controlled input

--- src/2/index.tsx
+++ src/2/index.tsx
@@ -1,15 +1,19 @@
-import { FunctionComponent, useState } from "react";
+import { ChangeEvent, FunctionComponent, useState } from "react";
 
 const Task2: FunctionComponent = () => {
   const [value, setValue] = useState("");
 
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div>
       <label>Controlled Input: </label>
-      <input />
+      <input onChange={handleChange} value={value} />
       <br />
       <br />
-      <p>#Show Input value here#</p>
+      <p>{value}</p>
     </div>
   );
 };
