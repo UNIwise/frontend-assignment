07f45e9bbcf54a49c464a35164e04817a643bc3a
Arwa mohammed <arwa.elmowafy1996@gmail.com>
2022-09-27 21:15:49 +0200

    refactor(task3): trim spaces in items

--- src/3/index.tsx
+++ src/3/index.tsx
@@ -1,17 +1,32 @@
-import { FunctionComponent, useState } from "react";
+import { ChangeEvent, FunctionComponent, useState } from "react";
+import { items } from "../1";
 
 const Task3: FunctionComponent = () => {
   const [inputValue, setInputValue] = useState("");
-  const [items, setItems] = useState([]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const selectedItems = items
+    .filter((val) => {
+      return val.itemName
+        .replace(" ", "")
+        .toLowerCase()
+        .includes(inputValue.replace(" ", "").toLowerCase());
+    })
+    .map((item) => {
+      return <div key={item.id}>{item.itemName}</div>;
+    });
 
   return (
-    <div>
+    <>
       <label>Search Input: </label>
-      <input />
+      <input value={inputValue} onChange={handleChange} />
       <br />
       <br />
-      #List goes here#
-    </div>
+      <div>{selectedItems}</div>
+    </>
   );
 };
 
